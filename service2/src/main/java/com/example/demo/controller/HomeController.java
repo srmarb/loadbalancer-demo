25f73ef1b419279db33611a9d08d6eb6f5604242
srmarb <marcos.barbosa@mercadolivre.com>
2023-01-16 18:14:22 -0300

    poc redirect with nginx

--- service2/src/main/java/com/example/demo/controller/HomeController.java
+++ service2/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,20 +1,26 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/{id}")
 public class HomeController {
 
     @Value("${test}")
     private String test;
 
     @GetMapping
-    public String test() {
-        return "OK" + test;
+    public ResponseEntity<?> test(@PathVariable final Integer id) {
+        if(id == 10) {
+            return new ResponseEntity(HttpStatus.I_AM_A_TEAPOT);
+        }
+        return ResponseEntity.ok("OK" + test);
     }
 
 }
