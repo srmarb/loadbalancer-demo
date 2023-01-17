20e65135944b9c5d0b288a6d7d32fb832665e4e7
srmarb <marcos.barbosa@mercadolivre.com>
2023-01-16 22:49:17 -0300

    poc redirect with nginx

--- service1/src/main/java/com/example/demo/controller/HomeController.java
+++ service1/src/main/java/com/example/demo/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class HomeController {
     private String test;
 
     @GetMapping
-    public String test() {
+    public String test(@PathVariable final String id) {
         return "OK" + test;
     }
 
